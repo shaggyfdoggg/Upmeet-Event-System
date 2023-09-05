fd155de77d6688795f40d7a14445c27edcd86fa4
Zach Buth <zachbuth@gmail.com>
2023-09-05 17:08:45 -0400

    End of Day1

--- Upmeet Event System/ClientApp/src/app/services/event.service.ts
+++ Upmeet Event System/ClientApp/src/app/services/event.service.ts
@@ -1,9 +1,36 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Event } from '../models/event';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventService {
 
-  constructor() { }
+  constructor(private http:HttpClient, @Inject('BASE_URL') private baseUrl: string) { }
+
+GetEvents():Observable<Event[]>{
+  return this.http.get<Event[]>(`${this.baseUrl}api/Event`);
 }
+
+GetEventById(id:number):Observable<Event>{
+  return this.http.get<Event>(`${this.baseUrl}api/Event/${id}`);
+}
+
+AddEvent(newEvent:Event):Observable<Event>{
+  return this.http.post<Event>(`${this.baseUrl}api/Event`, newEvent);
+}
+
+DeleteEvent(id:number){
+  return this.http.delete<Event>(`${this.baseUrl}api/Event/${id}`);
+}
+
+UpdateEvent(updatedEvent: Event): Observable<Event>{
+  return this.http.put<Event>(`$(this.baseUrl)api/Order/${updatedEvent.id}`, updatedEvent);
+}
+}
+
+
+
+
